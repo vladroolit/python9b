5b436a52b6dfe1029e3f185b80f605bca1915e36
vladroolit <52249590+vladroolit@users.noreply.github.com>
2020-08-08 12:56:28 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,417 @@
+cop = input ('прежде чем использовать данную базу я надеюсь что вы прочтете это\nваши дальнейшие действия полностью незаконы ибо вы имейте доступ к личтой информаций других людей\n и да автор этой штуки далбоеб и конченый упырь и не советует вам использовать его творение\n и как и он вы тоже далбоеб так как используете его творение с наилучшими пожеланиями от автора идите нахуй упыри ебанные\n если вы согласны начать использовать это приложение то нажмите y если нет то n: ')
+
+if cop == 'y':
+
+	nam = input ('введите имя' +  "(" + 'имя должно быть полное с первыми заглавными буквами фамилиа должна быть первой и без ошибок иначе вылезет ебучая ошибка ' + ")" + ': ')
+
+	class Person:
+		name  = 'valera'
+		age   = '08.08.2020'
+		tlphn = '+37368895706' 
+
+		def set(self):
+			self.name  = name
+			self.age   = age
+			self.tlphn = tlphn
+
+	lora = Person()
+	lora.name  = 'Belbas Larisa'
+	lora.age   = '08.05.2006'
+	lora.tlphn = '+37378863303'
+
+	vera = Person()
+	vera.name  = 'Bucataru Vera'
+	vera.age   = '01.01.1963'
+	vera.tlphn = '+37369610514'
+
+	sanda = Person()
+	sanda.name  = 'Paiu Alexandra'
+	sanda.age   = '09.02.2006'
+	sanda.tlphn = '+37379871942'
+
+	alina = Person()
+	alina.name  = 'Taraburca Alina'
+	alina.age   = '19.10.2005'
+	alina.tlphn = '+37360084633'
+
+	tolea = Person()
+	tolea.name  = 'Graur Anatol'
+	tolea.age   = '29.11.2005'
+	tolea.tlphn = '+37360513517'
+
+	cotl = Person()
+	cotl.name  = 'Cotelea Andrei'
+	cotl.age   = '15.09.2005'
+	cotl.tlphn = '+37361053781'
+
+	toha = Person()
+	toha.name  = 'Grubneac Anton'
+	toha.age   = '29.12.2005'
+	toha.tlphn = '+37368219255'
+
+	bogdan = Person()
+	bogdan.name  = 'Turcanu Bogdan'
+	bogdan.age   = '25.07.2006'
+	bogdan.tlphn = '+37369610514'
+
+	miha = Person()
+	miha.name  = 'Caldare Mihai'
+	miha.age   = '11.08.2006'
+	miha.tlphn = '+37368699385'
+
+	valera = Person()
+	valera.name  = 'Cazac Valeria'
+	valera.age   = 'темноя лошадка интернета информация не найдена'
+	valera.tlphn = '+37369840333'
+
+	dana = Person()
+	dana.name  = 'Romanov Dana'
+	dana.age   = '19.12.2005'
+	dana.tlphn = '+37379916992'
+
+
+	butuc = Person()
+	butuc.name  = 'Butucel Daniel'
+	butuc.age   = '16.04.1992'
+	butuc.tlphn = '+37369572665'
+
+	merenic = Person()
+	merenic.name  = 'Mereniuc Daniel'
+	merenic.age   = '28.09.2005'
+	merenic.tlphn = '+37378127345'
+
+	nico = Person()
+	nico.name  = 'Nicorici David'
+	nico.age   = '07.10.2005'
+	nico.tlphn = '+37360507714'
+
+	emi = Person()
+	emi.name  = 'Oleinic Emanuel'
+	emi.age   = 'темноя лошадка интернета информация не найдена'
+	emi.tlphn = '+37378321955'
+
+	ginga = Person()
+	ginga.name  = 'Ginga David'
+	ginga.age   = '24.03.2005'
+	ginga.tlphn = '+37378838311'
+
+	ina = Person()
+	ina.name  = 'Frolov Ina'
+	ina.age   = '22.04.2006'
+	ina.tlphn = '+37360022918'
+
+	leo = Person()
+	leo.name  = 'Turcanu Leonard'
+	leo.age   = '11.08.2005'
+	leo.tlphn = '+37367733445'
+
+	lili = Person()
+	lili.name  = 'Plamadeala Lilian'
+	lili.age   = '14.06.2005'
+	lili.tlphn = '+37360026009'
+
+	masa = Person()
+	masa.name  = 'Stirbul Maria'
+	masa.age   = '14.11.2005'
+	masa.tlphn = '+37361034250'
+
+	mari = Person()
+	mari.name  = 'Bogos Mariana'
+	mari.age   = '21.09.2005'
+	mari.tlphn = '+37369017425'
+
+	nast = Person()
+	nast.name  = 'Martin Anastasia'
+	nast.age   = '15.11.2005'
+	nast.tlphn = '+37368870472'
+
+	milk = Person()
+	milk.name  = 'Orosan Milean'
+	milk.age   = '10.03.2005'
+	milk.tlphn = '+37368004539'
+
+	niga = Person()
+	niga.name  = 'Negru Eugen'
+	niga.age   = '12.08.2006'
+	niga.tlphn = '+37379792852'
+
+	niki = Person()
+	niki.name  = 'Celac Nikita'
+	niki.age   = '22.10.2005'
+	niki.tlphn = '+37360955700'
+
+	cris = Person()
+	cris.name  = 'Novac Cristina'
+	cris.age   = '10.08.2005'
+	cris.tlphn = '+37360280866'
+
+	oliv = Person()
+	oliv.name  = 'Prodan Olivia'
+	oliv.age   = '12.04.2006'
+	oliv.tlphn = '+37369997546'
+
+	ovid = Person()
+	ovid.name  = 'Onofriescu Ovidiu'
+	ovid.age   = 'темноя лошадка интернета информация не найдена'
+	ovid.tlphn = '+37368158099'
+
+	simo = Person()
+	simo.name  = 'Ciobanu Simona'
+	simo.age   = 'темноя лошадка интернета информация не найдена'
+	simo.tlphn = '+37367681519'
+
+	stas = Person()
+	stas.name  = 'Caracevtev Stas Pidaras'
+	stas.age   = '20.09.2005'
+	stas.tlphn = '+37379154113'
+
+	zina = Person()
+	zina.name  = 'Ungureanu Zinaida'
+	zina.age   = 'темноя лошадка интернета информация не найдена'
+	zina.tlphn = '+37368438117'
+
+	vale = Person()
+	vale.name  = 'Iacobuta Valentina'
+	vale.age   = '16.04.2006'
+	vale.tlphn = '+37368479748'
+
+	bale = Person()
+	bale.name  = 'Buliga Vlad'
+	bale.age   = '07.08.2006'
+	bale.tlphn = '+37360025372'
+
+	vlada = Person()
+	vlada.name  = 'Grosu Vlada'
+	vlada.age   = 'темноя лошадка интернета информация не найдена'
+	vlada.tlphn = '+37379383590'
+
+	valer = Person()
+	valer.name  = 'Ungureanu Valeria'
+	valer.age   = '13.09.2005'
+	valer.tlphn = '+37378371991'
+
+	doua = Person()
+	doua.name  = 'Gherega Stefan'
+	doua.age   = '13.09.2005'
+	doua.tlphn = '+37369552555'
+
+	tina = Person()
+	tina.name  = 'Ciobanu Tina'
+	tina.age   = '12.02.2006'
+	tina.tlphn = '+37379234236'
+
+	bobi = Person()
+	bobi.name  = 'Bobeico Tamara'
+	bobi.age   = '02.07.1957'
+	bobi.tlphn = '+37369594296'
+
+	vlad = Person()
+	vlad.name  = 'Popa Vlad'
+	vlad.age   = 'нет инфы'
+	vlad.tlphn = 'пошли нахуй'
+
+
+
+	if nam == lora.name:
+		print('Полное имя: ' + lora.name) 
+		print('Дата рождения:' + lora.age)
+		print('Телефон:' + lora.tlphn)
+
+	if nam == vera.name:
+		print('Полное имя: ' + vera.name) 
+		print('Дата рождения:' + vera.age)
+		print('Телефон:' + vera.tlphn)
+
+	if nam == sanda.name:
+		print('Полное имя: ' + sanda.name) 
+		print('Дата рождения:' + sanda.age)
+		print('Телефон:' + sanda.tlphn)
+
+	if nam == alina.name:
+		print('Полное имя: ' + alina.name) 
+		print('Дата рождения:' + alina.age)
+		print('Телефон:' + alina.tlphn)
+
+	if nam == tolea.name:
+		print('Полное имя: ' + tolea.name) 
+		print('Дата рождения:' + tolea.age)
+		print('Телефон:' + tolea.tlphn)
+
+	if nam == cotl.name:
+		print('Полное имя: ' + cotl.name) 
+		print('Дата рождения:' + cotl.age)
+		print('Телефон:' + cotl.tlphn)
+
+	if nam == toha.name:
+		print('Полное имя: ' + toha.name) 
+		print('Дата рождения:' + toha.age)
+		print('Телефон:' + toha.tlphn)
+
+	if nam == bogdan.name:
+		print('Полное имя: ' + bogdan.name) 
+		print('Дата рождения:' + bogdan.age)
+		print('Телефон:' + bogdan.tlphn)
+
+	if nam == miha.name:
+		print('Полное имя: ' + miha.name) 
+		print('Дата рождения:' + miha.age)
+		print('Телефон:' + miha.tlphn)
+
+	if nam == valera.name:
+		print('Полное имя: ' + valera.name) 
+		print('Дата рождения:' + valera.age)
+		print('Телефон:' + valera.tlphn)
+
+	if nam == dana.name:
+		print('Полное имя: ' + dana.name) 
+		print('Дата рождения:' + dana.age)
+		print('Телефон:' + dana.tlphn)
+
+	if nam == butuc.name:
+		print('Полное имя: ' + butuc.name) 
+		print('Дата рождения:' + butuc.age)
+		print('Телефон:' + butuc.tlphn)
+
+	if nam == merenic.name:
+		print('Полное имя: ' + merenic.name) 
+		print('Дата рождения:' + merenic.age)
+		print('Телефон:' + merenic.tlphn)
+
+	if nam == nico.name:
+		print('Полное имя: ' + nico.name) 
+		print('Дата рождения:' + nico.age)
+		print('Телефон:' + nico.tlphn)
+
+	if nam == emi.name:
+		print('Полное имя: ' + emi.name) 
+		print('Дата рождения:' + emi.age)
+		print('Телефон:' + emi.tlphn)
+
+	if nam == ginga.name:
+		print('Полное имя: ' + ginga.name) 
+		print('Дата рождения:' + ginga.age)
+		print('Телефон:' + ginga.tlphn)
+
+	if nam == ina.name:
+		print('Полное имя: ' + ina.name) 
+		print('Дата рождения:' + ina.age)
+		print('Телефон:' + ina.tlphn)
+
+	if nam == valera.name:
+		print('Полное имя: ' + valera.name) 
+		print('Дата рождения:' + valera.age)
+		print('Телефон:' + valera.tlphn)
+
+	if nam == leo.name:
+		print('Полное имя: ' + leo.name) 
+		print('Дата рождения:' + leo.age)
+		print('Телефон:' + leo.tlphn)
+
+	if nam == lili.name:
+		print('Полное имя: ' + lili.name) 
+		print('Дата рождения:' + lili.age)
+		print('Телефон:' + lili.tlphn)
+
+	if nam == masa.name:
+		print('Полное имя: ' + masa.name) 
+		print('Дата рождения:' + masa.age)
+		print('Телефон:' + masa.tlphn)
+
+	if nam == mari.name:
+		print('Полное имя: ' + mari.name) 
+		print('Дата рождения:' + mari.age)
+		print('Телефон:' + mari.tlphn)
+
+	if nam == nast.name:
+		print('Полное имя: ' + nast.name) 
+		print('Дата рождения:' + nast.age)
+		print('Телефон:' + nast.tlphn)
+
+	if nam == milk.name:
+		print('Полное имя: ' + milk.name) 
+		print('Дата рождения:' + milk.age)
+		print('Телефон:' + milk.tlphn)
+
+	if nam == niki.name:
+		print('Полное имя: ' + niki.name) 
+		print('Дата рождения:' + niki.age)
+		print('Телефон:' + niki.tlphn)
+
+	if nam == niga.name:
+		print('Полное имя: ' + niga.name) 
+		print('Дата рождения:' + niga.age)
+		print('Телефон:' + niga.tlphn)
+
+	if nam == cris.name:
+		print('Полное имя: ' + cris.name) 
+		print('Дата рождения:' + cris.age)
+		print('Телефон:' + cris.tlphn)
+
+	if nam == oliv.name:
+		print('Полное имя: ' + oliv.name) 
+		print('Дата рождения:' + oliv.age)
+		print('Телефон:' + oliv.tlphn)
+
+	if nam == ovid.name:
+		print('Полное имя: ' + ovid.name) 
+		print('Дата рождения:' + ovid.age)
+		print('Телефон:' + ovid.tlphn)
+
+	if nam == simo.name:
+		print('Полное имя: ' + simo.name) 
+		print('Дата рождения:' + simo.age)
+		print('Телефон:' + simo.tlphn)
+
+	if nam == stas.name:
+		print('Полное имя: ' + stas.name) 
+		print('Дата рождения:' + stas.age)
+		print('Телефон:' + stas.tlphn)
+
+	if nam == zina.name:
+		print('Полное имя: ' + zina.name) 
+		print('Дата рождения:' + zina.age)
+		print('Телефон:' + zina.tlphn)
+
+	if nam == vale.name:
+		print('Полное имя: ' + vale.name) 
+		print('Дата рождения:' + vale.age)
+		print('Телефон:' + vale.tlphn)
+
+	if nam == bale.name:
+		print('Полное имя: ' + bale.name) 
+		print('Дата рождения:' + bale.age)
+		print('Телефон:' + bale.tlphn)
+
+	if nam == valer.name:
+		print('Полное имя: ' + valer.name) 
+		print('Дата рождения:' + valer.age)
+		print('Телефон:' + valer.tlphn)
+
+	if nam == doua.name:
+		print('Полное имя: ' + doua.name) 
+		print('Дата рождения:' + doua.age)
+		print('Телефон:' + doua.tlphn)
+
+	if nam == vlada.name:
+		print('Полное имя: ' + vlada.name) 
+		print('Дата рождения:' + vlada.age)
+		print('Телефон:' + vlada.tlphn)
+
+	if nam == tina.name:
+		print('Полное имя: ' + tina.name) 
+		print('Дата рождения:' + tina.age)
+		print('Телефон:' + tina.tlphn)
+
+	if nam == bobi.name:
+		print('Полное имя: ' + bobi.name) 
+		print('Дата рождения:' + bobi.age)
+		print('Телефон:' + bobi.tlphn)
+
+	if nam == vlad.name:
+		print('Полное имя: ' + vlad.name) 
+		print('Дата рождения:' + vlad.age)
+		print('Телефон:' + vlad.tlphn)
+		print('Доп информация: автор этой ебучей хуйни которому нехуй делать в свободное время')
+
+else: 
+	print ('ну и иди ты нахуй')
